556e4ce3c32dde1460e4265bf03a62243ae9b4c0
Aleksandr Terekhov <a_terexov@mail.ru>
2022-11-08 21:24:02 +0500

    added cycle

--- training.py
+++ training.py
@@ -1,8 +1,14 @@
-number_1 = int(input('Введите первое число: '))
-number_2 = int(input('Введите второе число: '))
-if number_1 > number_2:
-    print(f'{number_1} больше {number_2}')
-elif number_2 > number_1:
-    print(f'{number_2} больше {number_1}')
+def check_number(number):
+    return count_number_len(number) > 4
+
+def count_number_len(x):
+    count = 0
+    while x:
+        count += 1
+        x //= 10
+
+x = 1234
+if check_number(x):
+    print("1")
 else:
-    print('Числа равны')
+    print("2")
